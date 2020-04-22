be44624d4e6ecdc4e80b004698c038569d10a702
Suman Sigdel <53033648+sumansid@users.noreply.github.com>
2020-04-22 12:45:41 -0400

    Added Pokemon for variant

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ addEventListener('fetch', event => {
 */
 
 const url = "https://cfw-takehome.developers.workers.dev/api/variants";
-const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/pikachu"
-const pokemonUrl2 = "https://pokeapi.co/api/v2/pokemon/ditto"
-const githubURL = "https://wwww.github.com/users/sumansid";
+const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/"
+const githubURL = "https://www.github.com/sumansid";
+const madeBy = "Made by Suman Sigdel"
 
 
 //Generate random number
@@ -19,57 +19,86 @@ function getrand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 async function handleRequest(request) {
-	
 	let variants = await fetch(url).then((resp)=>{return resp.json()});
-	let pikachuinfo = await fetch(pokemonUrl).then((resp)=>{return resp.json()});
-	let dittoinfo = await fetch(pokemonUrl2).then((resp)=>{return resp.json()});
-	
 	let varArray = variants.variants
-	
-	console.log("reqeust headers", request.headers.get("Cookie"))
-	//let cookies = request.headers.get("Cookie")
 	let cIndex = getCookie(request, "url")
-	if(cIndex != null){
-		console.log("loading from cookie index ", cIndex)
-		var single_url = varArray[cIndex];
-		let response = await fetch(single_url);
-		const elementHandler = new ElementHandler(dittoinfo.name)
-		const elementDescription = new ElementHandler(dittoinfo.order)
-		response = new HTMLRewriter()
-		.on("h1#title", elementHandler)//we wants to change the element with ID title
-		.on("p#description", elementDescription)
-		.transform(response)
-
-		return response;
+    if(cIndex != null){
+		let url = varArray[cIndex]
+		let response = await fetch(url);
+        return buildResponse(response, cIndex);
 	}else{
 		let index = getrand(0, varArray.length-1)
-		var single_url = varArray[index]
-		let response = await fetch(single_url);
+        let url = varArray[index];
+		let response = await fetch(url);
 		response = setCookie("url", index, response)
-		const elementHandler = new ElementHandler(pikachuinfo.name)
-		const elementDescription = new ElementHandler(pikachuinfo.order)
-		const elementURL = new ElementHandler(githubURL)
-		response = new HTMLRewriter()
-		.on("h1#title", elementHandler)
-		.on("p#description", elementDescription)
-		.on("a#url", elementURL)
-		.on("href", elementURL)
-		.transform(response)
-		return response;
+		return buildResponse(response, index);
 	}
-
 }
+
+async function buildResponse(response, index){
+    let pokemonIndex = parseInt(index)+1;
+    let pokemonIndexUrl = pokemonUrl + "/" + pokemonIndex;
+    let pokemon = await fetch(pokemonIndexUrl).then((resp)=>{return resp.json()});
+    const elementTitle = new ElementHandler("Variant " + pokemonIndex + " - " + madeBy)
+    const elementName = new ElementHandler(pokemon.name)
+	const elementDescription = new ElementHandler(madeBy)
+	const elementURL = new LinkHandler("My github", githubURL)
+    const elementSprite = new ImageHandler("Pokemon", pokemon.sprites.front_shiny)
+	response = new HTMLRewriter()
+    .on("title", elementTitle)
+	.on("h1#title", elementName)
+	.on("p#description", elementDescription)
+	.on("a#url", elementURL)
+    .on("svg", elementSprite)
+	.transform(response)
+    return response;
+}
+
 
 class ElementHandler {
 	//here in the constructor, we can receive some information from request
 	constructor(value){
 		this.value = value
 	}
+	//and here wwe will replace some attribute from DOM element
 	element(element) {
-		
+		//we can use any DOM selector
+		//the innerHTML change the content of element
 		element.setInnerContent(this.value)
+	}
+}
+
+class LinkHandler {
+	//here in the constructor, we can receive some information from request
+	constructor(value, link){
+		this.value = value
+        this.link = link;
+	}
+	//and here wwe will replace some attribute from DOM element
+	element(element) {
+		//we can use any DOM selector
+		//the innerHTML change the content of element
+		element.setInnerContent(this.value)
+        element.setAttribute("href", this.link)
+        element.setAttribute("target", "_blank")
+	}
+}
+
+class ImageHandler {
+	//here in the constructor, we can receive some information from request
+	constructor(alt, url){
+		this.alt = alt
+        this.url = url;
+	}
+	//and here wwe will replace some attribute from DOM element
+	element(element) {
+		//we can use any DOM selector
+		//the innerHTML change the content of element
+		element.tagName = "img"
+        element.setAttribute("src", this.url)
+        element.setAttribute("alt", this.alt)
+        element.setAttribute("class", "")
 	}
 }
 
